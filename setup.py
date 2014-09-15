8a647a41e2cc0b370dc5dd3fbce55b20d7713a34
Jason Hite <jasonmhite@gmail.com>
2014-09-15 14:32:56 -0400

    Make a proper python package to make things easier

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='ne770-sampling',
+    version='0.0.1',
+    author='Jason M. Hite',
+    author_email='jmhite@ncsu.edu',
+    packages=['sampling'],
+    description='Sampling tools for NE770',
+)
